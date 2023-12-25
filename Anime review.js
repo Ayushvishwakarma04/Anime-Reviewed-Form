49f7da08a5a433d1cf143f4d87b6c4e80995c577
Ayush Vishwakarma <143516401+Ayushvishwakarma04@users.noreply.github.com>
2023-12-25 22:26:53 +0530

    Update Anime review.js

--- Anime review.js
+++ Anime review.js
@@ -34,6 +34,16 @@ document.addEventListener("DOMContentLoaded", function() {
       console.error(error);
     });
   }
+
+  <a href="#" onclick="signOut();">Sign out</a>
+<script>
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+</script>
   
   function signOut() {
     googleAuth.signOut().then(function () {
